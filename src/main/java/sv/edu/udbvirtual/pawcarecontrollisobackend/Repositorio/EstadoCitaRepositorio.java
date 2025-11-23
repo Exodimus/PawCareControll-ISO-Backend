0b98da233a86c15ef6dacd296010a723471c09e6
Josepo616 <josemejia61976751@gmail.com>
2025-11-22 22:32:15 -0600

    feat: estado citas repository added

---
+++ src/main/java/sv/edu/udbvirtual/pawcarecontrollisobackend/Repositorio/EstadoCitaRepositorio.java
@@ -0,0 +1,7 @@
+package sv.edu.udbvirtual.pawcarecontrollisobackend.Repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+import sv.edu.udbvirtual.pawcarecontrollisobackend.Model.EstadoCitasModel;
+
+public interface EstadoCitaRepositorio extends CrudRepository<EstadoCitasModel, Long> {
+}
