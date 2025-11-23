c310a9b1fbe549e00f1b314be5ad7320c0754bb0
Josepo616 <josemejia61976751@gmail.com>
2025-11-22 22:32:54 -0600

    feat: estado citas service & implementation added

---
+++ src/main/java/sv/edu/udbvirtual/pawcarecontrollisobackend/Servicios/EstadoCitaServicioImple.java
@@ -0,0 +1,44 @@
+package sv.edu.udbvirtual.pawcarecontrollisobackend.Servicios;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sv.edu.udbvirtual.pawcarecontrollisobackend.Model.EstadoCitasModel;
+import sv.edu.udbvirtual.pawcarecontrollisobackend.Repositorio.EstadoCitaRepositorio;
+
+import java.util.ArrayList;
+import java.util.Optional;
+@Service
+public class EstadoCitaServicioImple implements EstadoCitaServicio{
+
+
+
+    @Autowired
+    EstadoCitaRepositorio estadoCitaRepositorio;
+    @Override
+    public ArrayList<EstadoCitasModel> getAllEstadoCita() {
+
+        return (ArrayList<EstadoCitasModel>) estadoCitaRepositorio.findAll();
+    }
+
+    @Override
+    public Optional<EstadoCitasModel> getEstadoCitaById(Long id) {
+        return estadoCitaRepositorio.findById(id);
+    }
+
+    @Override
+    public EstadoCitasModel GuardarEstadoCita(EstadoCitasModel a) {
+        return estadoCitaRepositorio.save(a);
+    }
+
+
+    @Override
+    public EstadoCitasModel ActualizarEstadoCita(Long id, EstadoCitasModel estadoCita) {
+        Optional<EstadoCitasModel> estadoCitaOptional = estadoCitaRepositorio.findById(id);
+        if (estadoCitaOptional.isPresent()) {
+            EstadoCitasModel estadoCitaExistente = estadoCitaOptional.get();
+            estadoCitaExistente.setNombre_Estado_Cita(estadoCita.getNombre_Estado_Cita());
+            return estadoCitaRepositorio.save(estadoCitaExistente);
+        }
+        return null; // o puedes lanzar una excepción si lo prefieres
+    }
+}
